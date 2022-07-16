a4fe430e4bb8a4514d130f92f3bd86c08c697852
Michał Jarmoliński <michal.jarmolinski@anchorgroup.software>
2022-07-16 18:33:51 +0200

    added detail module and meetup id pages && started creating api

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,52 @@
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails(props) {
+  return (
+    <MeetupDetail
+      address="some street 5, some city"
+      description="this is a first meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First meetup"
+    />
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        address: "some street 5, some city",
+        title: "first meetup",
+      },
+    },
+  };
+}
+
+export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,42 @@
-function HomePage() {
-  return <h1>The homepage</h1>;
+import MeetupList from "../components/meetups/MeetupList";
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Berlin, Prenzlauer Strasse",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "Second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Berlin, Prenzlauer Strasse",
+    description: "This is a second meetup",
+  },
+  {
+    id: "m3",
+    title: "Third meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Berlin, Prenzlauer Strasse",
+    description: "This is a third meetup",
+  },
+];
+
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 100,
+  };
 }
 
 export default HomePage;

2182b5cfd5f151831b12aa96894082b1d8b497c5
Michał Jarmoliński <michal.jarmolinski@anchorgroup.software>
2022-07-16 18:54:23 +0200

    added api route responsible for inserting documents into mongodb collection

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,29 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST/api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    // const { title, image, address, description } = data;
 
-    
+    const client = await MongoClient.connect(
+      "mongodb+srv://rhino11:admin@cluster0.8pxyd.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne({ data });
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({
+      message: "Meetup inserted!",
+    });
   }
 }
 
